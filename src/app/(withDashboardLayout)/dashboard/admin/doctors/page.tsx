94a5fc0cb665d60ad0036460e69a0103b55b540b
mdYeasin-cmd <yeasin200011@gmail.com>
2024-05-28 21:26:15 +0600

    Add doctor feature on admin dashboard

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,10 +1,109 @@
-import React from "react";
+"use client";
+
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+import DoctorModal from "./components/DoctorModal";
+import {
+  useDeleteDoctorMutation,
+  useGetAllDoctorsQuery,
+} from "@/redux/api/doctorApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Link from "next/link";
+import EditIcon from "@mui/icons-material/Edit";
+import { toast } from "sonner";
+import { useDebounced } from "@/redux/hooks";
 
 const DoctorsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  // doctors search
+  const query: Record<string, any> = {};
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = searchTerm;
+  }
+
+  const { data, isLoading } = useGetAllDoctorsQuery({ ...query });
+  const doctors = data?.doctors;
+  const meta = data?.meta;
+  const [deleteDoctor] = useDeleteDoctorMutation();
+
+  const handleDelete = async (id: string) => {
+    // console.log(id);
+    try {
+      const res = await deleteDoctor(id).unwrap();
+      // console.log(res);
+      if (res?.id) {
+        toast.success("Doctor deleted successfully!!!");
+      }
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "contactNumber", headerName: "Contact Number", flex: 1 },
+    { field: "gender", headerName: "Gender", flex: 1 },
+    { field: "apointmentFee", headerName: "Appointment Fee", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <IconButton
+              onClick={() => handleDelete(row.id)}
+              aria-label="delete"
+            >
+              <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+            <Link href={`/dashboard/admin/doctors/edit/${row.id}`}>
+              <IconButton aria-label="delete">
+                <EditIcon />
+              </IconButton>
+            </Link>
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>
-      <h1>Doctors Page</h1>
-    </div>
+    <Box>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Button onClick={() => setIsModalOpen(true)}>Create Doctor</Button>
+        <DoctorModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField
+          onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
+          placeholder="Search Doctors"
+        />
+      </Stack>
+
+      {!isLoading ? (
+        <Box sx={{ mt: 2 }}>
+          <DataGrid rows={doctors} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </Box>
   );
 };
 
