a407ce2961c04da342454cd1e3599c39d021dba8
mdYeasin-cmd <yeasin200011@gmail.com>
2024-05-30 21:42:16 +0600

    Add doctor edit part

---
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
@@ -0,0 +1,177 @@
+"use client";
+
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { Gender } from "@/types/common";
+import { FieldValues } from "react-hook-form";
+import PHForm from "@/components/Forms/PHForm";
+import PHInput from "@/components/Forms/PHInput";
+import PHFullScreenModal from "@/components/Shared/PHModal/PHFullScreenModal";
+import {
+  useCreateDoctorMutation,
+  useGetDoctorQuery,
+} from "@/redux/api/doctorApi";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { toast } from "sonner";
+import PHSelectField from "@/components/Forms/PHSelectField";
+import { useRouter } from "next/navigation";
+
+type TParams = {
+  params: {
+    doctorId: string;
+  };
+};
+
+const DoctorUpdatePage = ({ params }: TParams) => {
+  const id = params?.doctorId;
+  const router = useRouter();
+
+  const { data, isLoading } = useGetDoctorQuery(id);
+  const [updateDoctor] = useCreateDoctorMutation();
+
+  const handleFormSubmit = async (values: FieldValues) => {
+    values.experience = Number(values.experience);
+    values.apointmentFee = Number(values.apointmentFee);
+    values.id = id;
+    // console.log({ id: values.id, body: values });
+
+    try {
+      const res = await updateDoctor({ id: values.id, body: values }).unwrap();
+      if (res?.id) {
+        toast.success("Doctor Updated Successfully!!!");
+        router.push("/dashboard/admin/doctors");
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  const defaultValues = {
+    email: data?.email || "",
+    name: data?.name || "",
+    contactNumber: data?.contactNumber || "",
+    address: data?.address || "",
+    registrationNumber: data?.registrationNumber || "",
+    gender: data?.gender || "",
+    experience: data?.experience || 0,
+    apointmentFee: data?.apointmentFee || 0,
+    qualification: data?.qualification || "",
+    currentWorkingPlace: data?.currentWorkingPlace || "",
+    designation: data?.designation || "",
+  };
+
+  return (
+    <Box>
+      <Typography variant="h5" component={"h5"}>
+        Update Doctor Inforamtion
+      </Typography>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <PHForm
+          onSubmit={handleFormSubmit}
+          defaultValues={data && defaultValues}
+        >
+          <Grid container spacing={2} sx={{ my: 5 }}>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="name"
+                label="Name"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="email"
+                type="email"
+                label="Email"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="contactNumber"
+                label="Contract Number"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="address"
+                label="Address"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="registrationNumber"
+                label="Registration Number"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="experience"
+                type="number"
+                label="Experience"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHSelectField
+                items={Gender}
+                name="gender"
+                label="Gender"
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="appointmentFee"
+                type="number"
+                label="ApointmentFee"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="qualification"
+                label="Qualification"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="currentWorkingPlace"
+                label="Current Working Place"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="designation"
+                label="Designation"
+                fullWidth={true}
+                sx={{ mb: 2 }}
+              />
+            </Grid>
+          </Grid>
+
+          <Button type="submit">Create</Button>
+        </PHForm>
+      )}
+    </Box>
+  );
+};
+
+export default DoctorUpdatePage;
